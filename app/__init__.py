e944b761fb12d3900349f453bfe3dd69f457a331
Ayesha Siddiqha <ayesha27msvp@gmail.com>
2025-06-26 20:34:43 +0530

    db fix

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,8 @@ def create_app():
     
     app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'static'))
     
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
